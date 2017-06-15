7bd3f810ad69dec678605d3b6adc21b957dedde4
Michael Ayzenberg <mickayz@Michaels-MBP.si.securityinnovation.com>
2017-06-15 12:44:47 -0700

    second attempt

--- firmware/trezor.c
+++ firmware/trezor.c
@@ -103,8 +103,8 @@ int main(void)
 	oledSetDebugLink(1);
 	//storage_reset(); // wipe storage if debug link
 	//storage_reset_uuid();
-	storage_commit();
-	storage_clearPinArea(); // reset PIN failures if debug link
+	//storage_commit();
+	//storage_clearPinArea(); // reset PIN failures if debug link
 #endif
 
 	oledDrawBitmap(40, 0, &bmp_logo64);
