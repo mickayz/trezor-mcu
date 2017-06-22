e308ff3090498a9099d1ab4664dcd3622d55426b
Michael Ayzenberg <mickayz@Michaels-MBP.si.securityinnovation.com>
2017-06-22 13:23:36 -0700

    update to firmware layout

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,5 @@
 scp ./output/bootloader-mbl1.3.1.elf mickayz@172.16.251.139:~/trezor/boots/
 scp ./output/trezor-mbl1.3.1.elf mickayz@172.16.251.139:~/trezor/boots/
 ./constructfirmware.py ./output/bootloader-mbl1.3.1.bin ./output/trezor-mbl1.3.1.bin
-scp firmout.dat mickayz@172.16.251.139:~/trezor/boots/
+scp firmout.bin mickayz@172.16.251.139:~/trezor/boots/
 
