977c5ad99f071a31315baca8e56f12c99def9c26
Michael Ayzenberg <mickayz@Michaels-MBP.si.securityinnovation.com>
2017-06-22 15:23:34 -0700

    fixed buf cast

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 ./firmware-docker-build.sh mbl1.3.1
-./bootloader-docker-build.sh mbl1.3.1
-scp ./output/bootloader-mbl1.3.1.elf mickayz@172.16.251.139:~/trezor/boots/
+# ./bootloader-docker-build.sh mbl1.3.1
+# scp ./output/bootloader-mbl1.3.1.elf mickayz@172.16.251.139:~/trezor/boots/
 scp ./output/trezor-mbl1.3.1.elf mickayz@172.16.251.139:~/trezor/boots/
 ./constructfirmware.py ./output/bootloader-mbl1.3.1.bin ./output/trezor-mbl1.3.1.bin
 scp firmout.bin mickayz@172.16.251.139:~/trezor/boots/
